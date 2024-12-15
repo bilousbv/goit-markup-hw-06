1e08902456e0560b6fd54128ecdb9ce73acafedb
bilousBohdan <bilousbv@gmail.com>
2024-12-15 13:50:59 +0200

    Added @2x images; Rewrite page styles to mobile first; Added tablet version; Added desktop version;

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,32 @@
 ;(() => {
-  const refs = {
+  const modalRefs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    modalOverlay: document.querySelector('[data-modal-overlay]'),
     modal: document.querySelector('[data-modal]'),
   }
 
-  refs.openModalBtn.addEventListener('click', toggleModal)
-  refs.closeModalBtn.addEventListener('click', toggleModal)
+  modalRefs.openModalBtn.addEventListener('click', toggleModal)
+  modalRefs.closeModalBtn.addEventListener('click', toggleModal)
+  modalRefs.modalOverlay.addEventListener('click', toggleModal)
+  modalRefs.modal.addEventListener('click', (e) => {
+    e.stopPropagation()
+  })
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-open')
+    modalRefs.modalOverlay.classList.toggle('is-open')
+  }
+
+  const menuRefs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  }
+
+  menuRefs.openMenuBtn.addEventListener('click', toggleMenu)
+  menuRefs.closeMenuBtn.addEventListener('click', toggleMenu)
+
+  function toggleMenu() {
+    menuRefs.menu.classList.toggle('is-open')
   }
 })()
